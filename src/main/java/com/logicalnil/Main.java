15125762e932feee905f45ae78b6ab8747bf4646
Swapnil Desai <mrdesai@Swapnils-Mac-mini.local>
2025-07-01 18:54:43 +0530

    Dice Project Completed

--- src/main/java/com/logicalnil/Main.java
+++ src/main/java/com/logicalnil/Main.java
@@ -6,6 +6,24 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        //        ******************Dice-Project*****************
+        Scanner scanner = new Scanner(System.in);
+        int sum=0;
+        Random random = new Random();
+        Dice dice = new Dice();
+        System.out.print("\nHow Many times you want to Roll the Die ? : ");
+        int rollCount = scanner.nextInt();
+        if (rollCount !=0){
+            for (int i=1; i<=rollCount; i++){
+                int j= random.nextInt(1,7);
+                dice.setRol(j);
+                System.out.printf("\n Die rolled %d \n",j);
+                dice.printRol();
+                sum +=j;
+            }
+        }else System.out.println("You did not roll a die");
 
+        System.out.println("\n Sum of all rolling attempt  : " + sum);
+        scanner.close();
   }
 }
