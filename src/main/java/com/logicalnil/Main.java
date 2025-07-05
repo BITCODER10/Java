a663ad65935867eb0c2b7e31e36a3092aa534362
Swapnil Desai <59367604+BITCODER10@users.noreply.github.com>
2025-07-05 15:18:54 +0530

    Revert "2D Array Project completed"

--- src/main/java/com/logicalnil/Main.java
+++ src/main/java/com/logicalnil/Main.java
@@ -6,14 +6,6 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        TwoDimArray TDArray = new TwoDimArray();
-        Random random = new Random();
-        TDArray.setColumn(random.nextInt(1,4));
-        TDArray.setRows(random.nextInt(1,4));
-        TDArray.setMatrix();
-        System.out.println("Rows : "+TDArray.getRows());
-        System.out.println("Columns : "+TDArray.getColumn());
-        TDArray.buildMatrix();
-        TDArray.displayMatrix();
-    }
+
   }
+}
