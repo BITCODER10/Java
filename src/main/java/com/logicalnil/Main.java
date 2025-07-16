273d4d2a8381e397ed059e3cadd380f101105d2d
Swapnil Desai <mrdesai@Swapnils-Mac-mini.local>
2025-07-16 21:59:13 +0530

    Cleaning main branch

--- src/main/java/com/logicalnil/Main.java
+++ src/main/java/com/logicalnil/Main.java
@@ -6,6 +6,8 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println();
+        System.out.println("Switch to different branches to check the java projects");
+        System.out.println();
   }
 }
