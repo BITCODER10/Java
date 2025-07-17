f13fac12eefe267fdfabe1ad86ce910417d4df7f
Swapnil Desai <mrdesai@Swapnils-Mac-mini.local>
2025-07-17 20:06:49 +0530

    Landing Main Java file

--- src/main/java/com/logicalnil/Main.java
+++ src/main/java/com/logicalnil/Main.java
@@ -7,7 +7,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println();
-        System.out.println("Switch to different branches to check the java projects");
         System.out.println();
+        System.out.println("********************************************************");
+        System.out.println("Switch to different branches to check the java projects");
+        System.out.println("********************************************************");
   }
 }
