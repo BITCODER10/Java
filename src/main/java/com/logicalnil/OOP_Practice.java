7b773ec2fe0e3b5317516b4f035c896d941e8967
Swapnil Desai <mrdesai@Swapnils-Mac-mini.local>
2025-07-17 19:23:16 +0530

    cleaning

--- src/main/java/com/logicalnil/OOP_Practice.java
+++
@@ -1,9 +0,0 @@
-package com.logicalnil;
-
-import java.util.PrimitiveIterator;
-
-public class OOP_Practice {
-    String car = "Tesla";
-    private String model = "Y";
-    public int count = 1;
-}
