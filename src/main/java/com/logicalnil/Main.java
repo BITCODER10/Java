bbb4afc35c5a64ef95b918046bb44ff21653f393
Swapnil Desai <mrdesai@Swapnils-Mac-mini.local>
2025-07-20 23:28:31 +0530

    User input is added

--- src/main/java/com/logicalnil/Main.java
+++ src/main/java/com/logicalnil/Main.java
@@ -6,7 +6,11 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Days day = Days.MONDAY;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Day of the Week");
+        String myDay = scanner.nextLine().toUpperCase();
+        Days day = Days.valueOf(myDay);
         switch (day){
             case FRIDAY,
                  THURSDAY,
